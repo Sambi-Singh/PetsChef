39b00f36a56489b25bf455f160c74f7a89cf613a
Sambi <sambisingh289@gmail.com>
2026-09-05 21:42:50 -0500

    Images now show up when a user uploads them! :D

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser';
+import multer from 'multer';
 
 const app = express()
 const port = 3000
 app.set('view engine', 'ejs');
 
+//mock database
+let posts = []
+
+//use multer to deal with file inputs
+const upload = multer({dest: 'public/user_images'})
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded())
 
@@ -14,12 +20,20 @@ app.listen(port, ()=>{
 app.get("/", (req,res)=>{
     res.render('index')
 })
-app.post("/submit", (req,res)=>{
+app.post("/submit", upload.single('file'),(req,res)=>{
     console.log(req.body)
-    res.render('index', {image_file: req.body.file, image_description: req.body.description})
+    console.log(req.file)
+    var imageUrl = '/user_images/' + req.file.filename
+    console.log(imageUrl)
+
+    const newPost = {postImage: imageUrl, postMessage: req.body.description}
+    posts.unshift(newPost)
+    console.log(posts)
+    
+    res.render('index', {uploads: posts })
 })
 
 app.patch('/api/users/:id',(req,res)=>{
-    
+
 
 })
