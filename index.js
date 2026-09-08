5d15ddf46ecd4037ca96fbe5b715d9ae8d600fbc
Sambi <sambisingh289@gmail.com>
2026-09-07 21:58:41 -0500

    added patch request, so users can edit post description

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 //mock database
 let posts = []
 
+//id count, use to increment id
+var id_num = 0
+
 //use multer to deal with file inputs
 const upload = multer({dest: 'public/user_images'})
 app.use(express.static('public'))
@@ -20,20 +23,52 @@ app.listen(port, ()=>{
 app.get("/", (req,res)=>{
     res.render('index')
 })
+
+//test to see if users outputs
+app.get("/submit/posts", (req,res)=>{
+    res.send(posts)
+
+})
 app.post("/submit", upload.single('file'),(req,res)=>{
     console.log(req.body)
     console.log(req.file)
     var imageUrl = '/user_images/' + req.file.filename
     console.log(imageUrl)
+    //make sure to use the date as the id for the post
+    id_num = id_num + 1
+    const id = id_num;
 
-    const newPost = {postImage: imageUrl, postMessage: req.body.description}
+    const newPost = {postImage: imageUrl, postMessage: req.body.description, postID: id}
     posts.unshift(newPost)
     console.log(posts)
+
+
+    
     
     res.render('index', {uploads: posts })
 })
 
-app.patch('/api/users/:id',(req,res)=>{
+app.patch('/submit/:postID',(req,res)=>{
+    const  {body, params: { postID}, } = req;
+
+    const parsedId = parseInt(postID) //if parsedID is not a number send error
+    if(isNaN(parsedId)){
+        return res.sendStatus(400);
+    }
+
+    const findPostIndex = posts.findIndex(
+        (post) => post.postID === parsedId
+    )
+
+    if (findPostIndex === -1){
+        return res.sendStatus(404);
+    }
+
+    posts[findPostIndex] = {...posts[findPostIndex],...body};
+
+    return res.sendStatus(200);
+
+
 
 
 })
